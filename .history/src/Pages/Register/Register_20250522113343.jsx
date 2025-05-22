d5e9abefed84f76cfae974948b00457397f9cc4d
syed bayzid <husains82849@gmail.com>
2025-05-22 15:03:36 +0600

    group creationg start

---
+++ .history/src/Pages/Register/Register_20250522113343.jsx
@@ -0,0 +1,113 @@
+import { useContext } from "react";
+import { AuthContext } from "../../Contexts/AuthContext";
+import toast from "react-hot-toast";
+import { Link, useNavigate } from "react-router";
+import { FcGoogle } from "react-icons/fc";
+
+
+const Register = () => {
+    const { createUser, createUserWithGoogle } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    // register with email and pass
+    const handleRegister = (e) => {
+        e.preventDefault();
+
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        // password check 
+        const isUpperCase = /[A-Z]/.test(password);
+        const isLowerCase = /[a-z]/.test(password);
+        const isLongEnough = password.length >= 6;
+        if (!isUpperCase) {
+            toast.error("Password must contain at least one uppercase letter");
+            return;
+        }
+
+        if (!isLowerCase) {
+            toast.error("Password must contain at least one lowercase letter");
+            return;
+        }
+
+        if (!isLongEnough) {
+            toast.error("Password must be at least 6 characters long");
+            return;
+        }
+
+        createUser(email, password)
+            .then(result => {
+                console.log(result.user);
+                toast.success("Registered successfully ✅");
+                form.reset();
+                navigate('/');
+
+            })
+            .catch((error) => {
+                console.log(error);
+                toast.error(error.message);
+            });
+    };
+
+    // google btn
+    const handleGoogleBtn = () => {
+        createUserWithGoogle()
+            .then(result => {
+                console.log(result.user);
+                toast.success("Registered with Google ✅");
+                navigate("/"); // ✅ redirect
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
+
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-base-200 px-4">
+            <div className="w-full max-w-md bg-white p-8 shadow-lg rounded-lg">
+                <h2 className="text-2xl font-bold mb-6 text-center">Register to HobbyHub</h2>
+                {/* Google Sign In Button */}
+                <button
+                    onClick={handleGoogleBtn}
+                    className="btn btn-outline btn-info mb-6 flex items-center justify-center gap-2 w-full"
+                    type="button"
+                >
+                    <FcGoogle size={24} /> Register with Google
+                </button>
+                <div className="flex items-center w-full my-4">
+                    <hr className="w-full dark:text-gray-600" />
+                    <p className="px-3 dark:text-gray-600">OR</p>
+                    <hr className="w-full dark:text-gray-600" />
+                </div>
+                <form onSubmit={handleRegister} className="space-y-4">
+                    <div>
+                        <label className="block mb-1 font-semibold">Name</label>
+                        <input type="text" name="name" required className="input input-bordered w-full" />
+                    </div>
+                    <div>
+                        <label className="block mb-1 font-semibold">Email</label>
+                        <input autoComplete="email" type="email" name="email" required className="input input-bordered w-full" />
+                    </div>
+                    <div>
+                        <label className="block mb-1 font-semibold">Photo URL</label>
+                        <input autoComplete="url" type="text" name="photoURL" required className="input input-bordered w-full" />
+                    </div>
+                    <div>
+                        <label className="block mb-1 font-semibold">Password</label>
+                        <input autoComplete="new-password" type="password" name="password" required className="input input-bordered w-full" />
+                    </div>
+                    <button type="submit" className="btn btn-primary w-full">Register</button>
+                </form>
+                <p className="text-center mt-4 text-sm">
+                    Already have an account?{" "}
+                    <Link to="/login" className="text-blue-600 hover:underline font-semibold">
+                        Log In
+                    </Link>
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
---
+++ .history/src/Routes/PrivateRoute_20250522113103.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useContext } from 'react';
+import { AuthContext } from '../Contexts/AuthContext';
+import { Navigate } from 'react-router';
+
+const PrivateRoute = ({ children }) => {
+    const { user, loading } = useContext(AuthContext);
+
+    if (loading) {
+        return (
+            <div className="min-h-screen flex justify-center items-center">
+                <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-violet-600"></div>
+            </div>
+        )
+    }
+
+    if (user && user?.email) {
+        return children;
+    }
+    return <Navigate to="/login"></Navigate>
+};
+
+export default PrivateRoute;
